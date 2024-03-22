e947bbb821f9c9a57a9940f92c703eb697d7fe89
Enzo Falcon <falcone134@gmail.com>
2024-03-22 18:45:37 -0500

    layout partially finished

--- app.vue
+++ app.vue
@@ -3,10 +3,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<style>
-#__nuxt {
-  height: 100%;
-  width: 100%;
-}
-</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,13 @@
-<template>
-  <div class="flex min-h-screen">
-    <aside class="min-w-[250px] overflow-y-auto bg-primary-color">
-      <UCard :ui="{
-        base: 'flex flex-col h-screen',
-        background: 'dark:bg-primary-950',
-        rounded: '',
-        body: { padding: '' },
-        footer: { base: 'mt-auto' }
-      }">
-        <template #header>
-          <h2>App Falcón</h2>
-        </template>
-        <UVerticalNavigation :links="links" :ui="{
-          active: 'dark:before:bg-secondary-color before:bg-secondary-color',
-        }" />
-        <template #footer>
-          <UButton block @click="logout">Cerrar sesión</UButton>
-        </template>
-      </UCard>
-    </aside>
-    <section class="flex w-full" id="content">
-      <main class="w-[900px] mx-auto my-8">
-        <slot />
-      </main>
-    </section>
-  </div>
-</template>
-
 <script lang="ts" setup>
-const links = [
-  { label: 'Inicio', icon: 'i-heroicons-home-solid', to: '/' },
-  { label: 'Inquilinos', icon: 'i-heroicons-user-group-solid', to: '/tenants' },
-  { label: 'Historial', icon: 'i-heroicons-clipboard-document-list-solid', to: '/history' },
-  { label: 'Servicios', icon: 'i-heroicons-light-bulb-solid', to: '/services' },
-  { label: 'Cuartos', icon: 'i-heroicons-home-solid', to: '/rooms' },
-]
+const route = useRoute()
+
+const links = ref([
+  { label: 'Inicio', icon: 'pi pi-home', route: '/' },
+  { label: 'Inquilinos', icon: 'pi pi-users', route: '/tenants' },
+  { label: 'Historial', icon: 'pi pi-history', route: '/history' },
+  { label: 'Servicios', icon: 'pi pi-bolt', route: '/services' },
+  { label: 'Cuartos', icon: 'pi pi-home', route: '/rooms' },
+])
 
 const { execute } = await useLazyFetch('/api/auth/logout', {
   server: false,
@@ -50,3 +23,95 @@ const logout = async () => {
   navigateTo('/login')
 }
 </script>
+
+<template>
+  <div class="main-container">
+    <aside id="sidebar">
+      <Card>
+        <template #title>App Falcón</template>
+        <template #content>
+          <Menu :model="links">
+            <template #item="{ item, props }">
+              <router-link v-slot="{ href, navigate }" :to="item.route" custom>
+                <a :class="{ 'active-menu': route.path === item.route }" v-ripple :href="href" v-bind="props.action" @click="navigate">
+                  <span :class="item.icon" />
+                  <span class="menu-label">{{ item.label }}</span>
+                </a>
+                </router-link>
+            </template>
+          </Menu>
+        </template>
+        <template #footer>
+          <Button @click="logout">Cerrar sesión</Button>
+        </template>
+      </Card>
+    </aside>
+    <section id="content">
+      <main class="w-[900px] mx-auto my-8">
+        <slot />
+      </main>
+    </section>
+  </div>
+</template>
+
+<style>
+/** Layout styles */
+.main-container {
+  display: flex;
+	width: 100%;
+	min-height: 100vh;
+}
+
+#sidebar {
+  display: flex;
+}
+
+#content {
+  display: flex;
+  flex: 1;
+}
+
+/** Sidebar Card styles */
+.p-card {
+  min-width: 300px;
+}
+
+.p-card-body {
+  display: flex;
+  height: 100%;
+  padding: 1.5rem .5rem;
+}
+
+.p-card-title, .p-card-footer {
+  display: flex;
+  justify-content: center;
+}
+
+.p-card-footer {
+  margin-top: auto;
+}
+
+.p-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+/** Menu styles */
+.p-menu {
+  border: none;
+}
+.p-menuitem-link {
+  display: flex;
+  align-items: center;
+}
+.menu-label {
+  margin-left: 10px;
+}
+.active-menu {
+  background-color: var(--primary-color);
+  color: var(--primary-color-text);
+}
+
+</style>
