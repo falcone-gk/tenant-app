c6f469acea4675be4c1bce4a5b1748498545e8e2
Enzo Falcon <falcone134@gmail.com>
2024-03-29 20:27:34 -0500

    add schema validation for tenant crud

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
     username: z.string().min(1, { message: 'Campo requerido' }),
     password: z.string().min(1, { message: 'Campo requerido' })
 })
+
+export const tenantSchema = z.object({
+    name: z.string().min(1, { message: 'Campo requerido' }),
+    createdAt: z.string().min(1, { message: 'Campo requerido' }),
+    rooms: z.array(z.number()),
+})
