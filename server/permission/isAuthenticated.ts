e3c6e683d0504a6f8352786c0e794b984bf61364
Enzo Falcon <falcone134@gmail.com>
2024-03-31 21:55:22 -0500

    New authentication method

---
+++ server/permission/isAuthenticated.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client"
+import jwt from 'jsonwebtoken'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+
+    const config = useRuntimeConfig()
+    const token = getCookie(event, 'token')
+
+    if (!token) {
+        throw createError({ status: 401, message: 'Unauthorized' })
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            token: token
+        },
+        select: {
+            id: true,
+            username: true,
+            role: true
+        }
+    })
+
+    if (!user) {
+        throw createError({ status: 401, message: 'Unauthorized' })
+    }
+
+    const secretKey = config.public.jwtSecretKey as string
+    try {
+        jwt.verify(token, secretKey)
+    } catch (e) {
+        throw createError({ status: 401, message: 'Unauthorized' })
+    }
+
+    event.context.user = user
+})
