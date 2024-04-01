8b2671d718de28bfe96e5e6bebf0db0cf8fcec8a
Enzo Falcon <falcone134@gmail.com>
2024-03-31 22:03:44 -0500

    temporary fix for logout

--- server/api/auth/logout.ts
+++ server/api/auth/logout.ts
@@ -3,14 +3,20 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
+  // TODO: Create a new way to logout because user is always undefined
+  // in this case.
   const user = event.context.user
-  await prisma.user.update({
-    where: {
-      id: user.id
-    },
-    data: {
-      token: null
-    }
-  })
+
+  if (user) {
+    await prisma.user.update({
+      where: {
+        id: user.id
+      },
+      data: {
+        token: null
+      }
+    })
+  }
+
   return createResponse(event, 'success', 200, undefined, 'Cierre de sesión exitoso')
 })
