3862615f590198f65aba8d7c7a0c23f9e9ebc1a9
Enzo Falcon <falcone134@gmail.com>
2024-04-01 20:35:09 -0500

    Make token non nullable

--- server/api/auth/logout.ts
+++ server/api/auth/logout.ts
@@ -1,22 +1,26 @@
 import { PrismaClient } from "@prisma/client"
+import isAuthenticated from "~/server/permission/isAuthenticated"
 
 const prisma = new PrismaClient()
 
-export default defineEventHandler(async (event) => {
-  // TODO: Create a new way to logout because user is always undefined
-  // in this case.
-  const user = event.context.user
+export default defineEventHandler({
+  onRequest: [isAuthenticated],
+  handler: async (event) => {
+    // TODO: Create a new way to logout because user is always undefined
+    // in this case.
+    const user = event.context.user
 
-  if (user) {
-    await prisma.user.update({
-      where: {
-        id: user.id
-      },
-      data: {
-        token: null
-      }
-    })
+    if (user) {
+      await prisma.user.update({
+        where: {
+          id: user.id
+        },
+        data: {
+          token: ''
+        }
+      })
+    }
+
+    return createResponse(event, 'success', 200, undefined, 'Cierre de sesión exitoso')
   }
-
-  return createResponse(event, 'success', 200, undefined, 'Cierre de sesión exitoso')
 })
