237336048cb2efed086625c7f5d1a487f9bb476b
Enzo Falcon <falcone134@gmail.com>
2024-04-03 16:35:39 -0500

    Create simple loading screen

---
+++ components/LoadingScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="props.status === 'pending' || props.status === 'idle'">
+    <p>Cargando...</p>
+  </div>
+  <div v-if="props.status === 'error'">
+    <p>Error</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Status = 'pending' | 'idle' | 'error'
+
+const props = defineProps<{
+  status: Status
+}>()
+</script>
+
+<style scoped></style>
