7c2488190045009bcd43e2994d08ee24807e6a97
Enzo Falcon <falcone134@gmail.com>
2024-04-12 01:44:13 -0500

    create new form to add tenant payment

---
+++ components/dialog/form/AmountPaid.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div>
+      <h3>Información de pago de servicio:</h3>
+      <p>
+        <strong>Inquilino:</strong> {{ body.tenant.name }} <br>
+        <strong>Servicio:</strong> {{ body.service.name }}
+      </p>
+    </div>
+    <form class="form">
+      <div class="form-group">
+        <label for="amount">Monto:</label>
+        <InputNumber id="amount" mode="currency" currency="PEN" v-model="body.amount" showButtons :min="0" disabled />
+      </div>
+      <div class="form-group">
+        <label for="amountPaid">Monto Pagado:</label>
+        <InputNumber id="amountPaid" mode="currency" currency="PEN" v-model="body.paidMount" showButtons :min="0"
+          :max="body.amount!" disabled />
+      </div>
+      <div class="form-group">
+        <label for="newPayment">Pago agregado:</label>
+        <InputNumber id="newPayment" mode="currency" currency="PEN" v-model="body.newPayment" showButtons
+          :min="-body.paidMount!" :max="body.amount! - body.paidMount!" />
+        <span class="p-error">{{ getError("newPayment") }}</span>
+      </div>
+      <Button type="submit" @click.prevent="onSubmit" label="Enviar" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { DynamicDialogInstance } from 'primevue/dynamicdialogoptions'
+import { addPaySchema } from '~/schemas';
+
+const emits = defineEmits(['submit'])
+const dialogRef = inject<Ref<DynamicDialogInstance>>('dialogRef')
+const toast = useToast()
+
+const body = ref<{
+  tenant: { id: number, name: string },
+  service: { id: number, name: string, unit: string },
+  amount: number | null,
+  paidMount: number | null,
+  newPayment: number,
+}>({
+  tenant: { id: 0, name: '' },
+  service: { id: 0, name: '', unit: '' },
+  amount: null,
+  paidMount: null,
+  newPayment: 0
+})
+
+const paymentId = ref(0)
+
+const { status, error: payError, execute } = await useFetch(
+  () => `/api/tenants/payments/pay/${paymentId.value}`, {
+  method: 'POST',
+  body: body,
+  server: false,
+  immediate: false,
+  watch: false
+})
+
+const { validate, errors, isValid, clearErrors, getError } = useValidation(
+  addPaySchema, body, {
+  mode: 'lazy',
+});
+
+const onSubmit = async () => {
+  await validate()
+  if (isValid.value) {
+    await execute()
+    if (status.value === 'success') {
+      emits('submit', status.value)
+    } else {
+      console.log(payError.value)
+    }
+  }
+}
+
+onMounted(() => {
+  const { bodyData, selectedRowId } = dialogRef?.value.data
+  if (bodyData) {
+    body.value = {
+      ...body.value,
+      tenant: bodyData.tenant,
+      service: bodyData.service,
+      amount: bodyData.amount,
+      paidMount: bodyData.paidMount
+    }
+  }
+  paymentId.value = selectedRowId
+})
+</script>
